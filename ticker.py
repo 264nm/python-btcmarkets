7851f8118938207a3ae5572720968580a156e43b
264nm <c.j.ward264nm@gmail.com>
2018-01-26 18:20:51 +1100

    Created a generalised CLI ticker tool that can do all combinations

--- ticker.py
+++ ticker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import click
 import time
 import os
 from APIClient import QueryAPI
@@ -11,7 +11,7 @@ from GetConfig import GetConfig
 config = GetConfig()
 
 def ticker(curr1,curr2):
-    endpoint = "market/" + curr1 + "/" + curr2 + "/tick"
+    endpoint = "market" + "/" + curr1 + "/" + curr2 + "/tick"
     r = (QueryAPI(config.api_host, endpoint, "get"))
 
     ask = r["bestAsk"]
@@ -42,10 +42,60 @@ def ticker(curr1,curr2):
 
     return p
 
+@click.command()
+@click.argument(
+    'tick', nargs=2, type=(click.Tuple([str, str]))
+)
 
-def main():
-    print (ticker("XRP", "AUD"))
-    print (ticker("XRP", "BTC"))
-    print (ticker("BTC", "AUD"))
-if __name__== "__main__":
-    main()
+def get_tick_option(tick):
+    """
+    This tool prints all the tickers available on BTCMarkets.net
+    Options Available:
+
+    BTCMarkets.net at this stage only allows trading using AUD and BTC as currency.
+
+    Options Available:
+
+    \b
+    AUD:
+        * BTC - BitCoin
+        * LTC - LiteCoin
+        * ETH - Ethereum
+        * ETC - Ethereum Classic
+        * XRP - Ripple
+        * BCH - Bitcoin Cash
+
+    \b
+    BTC:
+        * LTC - LiteCoin
+        * ETH - Ethereum
+        * ETC - Ethereum Classic
+        * XRP - Ripple
+        * BCH - Bitcoin Cash
+    \b
+    USAGE:
+        ./ticker.py <coin> <currency>
+    """
+
+    valid_coins=['BTC', 'LTC', 'ETH', 'ETC', 'XRP', 'BCH']
+    coin=tick[0]
+    coin=([valid_coin for valid_coin in valid_coins if coin == valid_coin])
+    if not coin:
+        click.echo('Error invalid coin. See --help for correct options')
+    else:
+        coin = str(coin).strip('[]')
+
+    valid_currencies=['BTC', 'AUD']
+    currency=tick[1]
+    currency=([valid_currency for valid_currency in valid_currencies if currency == valid_currency])
+    if not currency:
+         click.echo('Error - invalid currency. See --help for correct options')
+         exit()
+    else:
+         currency = str(currency).strip('[]')
+
+    res = ticker(curr1=tick[0], curr2=tick[1])
+    click.echo(res)
+
+if __name__ == '__main__':
+    get_tick_option()
