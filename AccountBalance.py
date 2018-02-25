6c95c798c68cb9f70e4d9852eb503e9d3f894ab3
264nm <c.j.ward264nm@gmail.com>
2018-02-26 07:11:17 +1100

    Beginnings of human readable account balance format

--- AccountBalance.py
+++ AccountBalance.py
@@ -2,7 +2,10 @@
 
 from APIClient import BuildRequest
 
-def main():
+def AmountToJSON(amount):
+    return float(amount / 1e8)
+
+def build_request():
     """ Build the request headers and pass to APIClient with below endpoint
     TODO: Add in functionality to pass options for the CLI.
     """
@@ -11,7 +14,18 @@ def main():
     query_api = BuildRequest(endpoint, "get",
             request_body=None)
     r = query_api.get_results()
-    print (r)
+    return r
+
+def main():
+    r = build_request()
+    for g in r:
+        currency = g["currency"]
+        balance = g["balance"]
+        balance_human = AmountToJSON(balance)
+        pendingFunds = g["pendingFunds"]
+        pendingFunds_human = AmountToJSON(pendingFunds)
+        print (str(currency) + " : " + str(balance_human) + " (" +
+                str(pendingFunds_human) + ")")
 
 if __name__ == "__main__":
     main()
