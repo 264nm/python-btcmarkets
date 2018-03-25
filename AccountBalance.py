3a897c8d6e7101383cc9d59b36acc593e74839ff
264nm <c.j.ward264nm@gmail.com>
2018-03-25 21:07:43 +1100

    Account balance click interface

--- AccountBalance.py
+++ AccountBalance.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 from APIClient import BuildRequest
+import click
+
+
 
 def AmountToJSON(amount):
     return float(amount / 1e8)
@@ -16,16 +19,40 @@ def build_request():
     r = query_api.get_results()
     return r
 
-def main():
-    r = build_request()
-    for g in r:
+def balance_human_read(res):
+    balance_human_list = []
+    for g in res:
         currency = g["currency"]
         balance = g["balance"]
         balance_human = AmountToJSON(balance)
         pendingFunds = g["pendingFunds"]
         pendingFunds_human = AmountToJSON(pendingFunds)
-        print (str(currency) + " : " + str(balance_human) + " (" +
-                str(pendingFunds_human) + ")")
+        acc_bal_hum = (str(currency) + " : " + str(balance_human) + " (Pending: " +
+            str(pendingFunds_human) + ")")
+        balance_human_list.append(acc_bal_hum)
+    return balance_human_list
+
+@click.command()
+
+@click.option(
+    '--human', '-h', 'formatting', flag_value='human', default=True,
+    help="Output account balances in a human readable format"
+)
+
+@click.option(
+    '--raw', '-r', 'formatting', flag_value='raw',
+    help="Output the account balances as raw json response body"
+)
+
+def get_account_balance(formatting):
+    r = build_request()
+    if formatting == 'human':
+        balances = balance_human_read(r)
+        output = "\n".join(balance for balance in balances)
+    if formatting == 'raw':
+        output = r
+
+    click.echo(output)
 
 if __name__ == "__main__":
-    main()
+    get_account_balance()
